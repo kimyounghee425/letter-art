919d1cb1d7dadd47fa484a3e3e15a586ba013be8
kimyounghee425 <doctorsean0425@gmail.com>
2025-05-25 01:26:37 +0900

    [feat] rgbToGray, pixelToChar 작성

--- src/core/pixelToChar.ts
+++ src/core/pixelToChar.ts
@@ -1,1 +1,14 @@
 // 픽셀 -> 문자 변환 로직
+
+export const rgbToGray = function (r: number, g: number, b: number): number {
+    // RGB 값을 회색값으로 바꾸는 공식
+    return 0.299 * r + 0.587 * b + 0.114 * b;
+};
+
+const charset = '@%#*+=-:.'; // 어두움 -> 밝음
+
+// 0~255 명암도를 대응 문자로 변환하는 함수
+export const pixelToChar = function (gray: number): string {
+    const index = Math.floor((gray / 255) * (charset.length - 1));
+    return charset[index];
+};
